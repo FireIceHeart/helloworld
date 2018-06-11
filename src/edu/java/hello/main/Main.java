1d08ba8ebf46b67f1abd9bd205763d3b0708f1b3
Administrator <Administrator@PC-20180111ICYY>
2018-06-11 16:10:03 +0800

    简单修改

--- src/edu/java/hello/main/Main.java
+++ src/edu/java/hello/main/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("123");
+		System.out.println("hello github");
 		int[] ints= new int[20];
 		for (int i = 0; i< ints.length;i++){
 			System.out.println("true");
